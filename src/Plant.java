fe30e0f5c667e74877180006130f45c5472dd385
Alzbeta V. <alzbeta.voldanova@gmail.com>
2025-12-26 22:00:37 +0100

    Ukol 1 + 2 - data, konstruktory, metody

---
+++ src/Plant.java
@@ -0,0 +1,77 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Plant {
+
+    private static final int DEFAULT_WATERING_FREQUENCY = 7;
+    private static final DateTimeFormatter CZECH_DATE = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    private String name;
+    private String notes;
+    private LocalDate planted;
+    private LocalDate watering;
+    private int frequencyOfWatering;
+
+    public Plant(String name, String notes, LocalDate planted, LocalDate watering, int frequencyOfWatering) {
+        this.name = name;
+        this.notes = notes;
+        this.planted = planted;
+        this.watering = watering;
+        this.frequencyOfWatering = frequencyOfWatering;
+    }
+
+    public Plant(String name, int frequencyOfWatering) {
+        this(name, "", LocalDate.now(), LocalDate.now(), frequencyOfWatering );
+    }
+
+    public Plant(String name) {
+        this(name, DEFAULT_WATERING_FREQUENCY );
+    }
+
+
+    public String getWateringInfo() {
+        return "Rostlina " + name +
+                " byla naposledy zalita " + formatDateCz(watering) +
+                " a další zaléváni je naplánováno na " + formatDateCz(nextWatering()) + ".";
+    }
+
+    private String formatDateCz(LocalDate date) {
+        return date.format(CZECH_DATE);
+    }
+
+    public LocalDate nextWatering() {
+        return watering.plusDays(frequencyOfWatering);
+    }
+
+    public void doWateringNow(){
+        this.watering = LocalDate.now();
+    }
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public String getNotes() {return notes;}
+    public void setNotes(String notes) {this.notes = notes;}
+
+    public LocalDate getPlanted() {return planted;}
+    public void setPlanted(LocalDate planted) {this.planted = planted;}
+
+    public LocalDate getWatering() {return watering;}
+    public void setWatering(LocalDate watering) {this.watering = watering;}
+
+    public int getFrequencyOfWatering() {return frequencyOfWatering;}
+    public void setFrequencyOfWatering(int frequencyOfWatering) {this.frequencyOfWatering = frequencyOfWatering;}
+
+    @Override
+    public String toString() {
+        return "Plant{" +
+                "name='" + name + '\'' +
+                ", notes='" + notes + '\'' +
+                ", planted=" + planted +
+                ", watering=" + watering +
+                ", frequencyOfWatering=" + frequencyOfWatering +
+                '}';
+    }
+}
+
+
---
+++ src/PlantException.java
@@ -0,0 +1,9 @@
+public class PlantException extends Exception {
+
+    public PlantException(String message) {
+        super(message);
+    }
+}
+
+
+
